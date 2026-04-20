3afb92b103230463d939ceb029d2c2acfc2425bc
venomhocs <josephprovenomyahoo@gmail.com>
2026-04-20 11:45:11 -0700

    Add basic input and addition functionality
    Fix indentation and syntax errors in hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("hello, world")
+input("what is your name?")
+print("now you try this")
+num1 = float(input("enter the first number:")
+             num2 = float(input("enter second number:)
+                                result = num1 + num2
+                                print("Result:", result)
